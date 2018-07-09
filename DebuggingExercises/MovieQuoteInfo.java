ebb270a7b3e2c48e054732c5ca1ca976c8bca65d
oakgarziwa <angusoakgarziwa@gmail.com>
2018-07-09 14:52:25 +0800

    Fixed ver2

---
+++ DebuggingExercises/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Favourite movie quote = May the force be with you");
+        System.out.println("The movie it comes from = Star Wars");
+        System.out.println("The character who said it = Obi-Wan Kenobi");
+        System.out.println("The year of the movie = 1977");
+    }
+}
